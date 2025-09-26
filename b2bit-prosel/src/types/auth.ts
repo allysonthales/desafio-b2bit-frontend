5043d347c91b859d057c0c925eae9ce1ad26a7b3
Allyson Thales <121051849+allysonthales@users.noreply.github.com>
2025-09-26 23:43:40 +0000

    Add login response api types

---
+++ b2bit-prosel/src/types/auth.ts
@@ -0,0 +1,27 @@
+export interface Avatar {
+	id: number;
+	high: string;
+	medium: string;
+	low: string;
+}
+
+export interface User {
+	id: number;
+	name: string;
+	email: string;
+	is_active: boolean;
+	avatar: Avatar;
+	type: string;
+	created: string;
+	modified: string;
+	role: string;
+}
+export interface Tokens {
+	refresh: string;
+	access: string;
+}
+
+export interface LoginResponse {
+	user: User;
+	tokens: Tokens;
+}
