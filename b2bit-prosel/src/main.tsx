ede8c6aee07a5609a2afa54f8060288249ef48e7
Allyson Thales <121051849+allysonthales@users.noreply.github.com>
2025-09-27 14:51:44 +0000

    Remove unused BrowserRouter import from main.tsx

--- b2bit-prosel/src/main.tsx
+++ b2bit-prosel/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router";
 
 const container = document.getElementById("root");
 
