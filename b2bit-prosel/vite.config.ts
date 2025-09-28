4a32b00274787cadcb25b4ccb64496d36f158e99
Allyson Thales <121051849+allysonthales@users.noreply.github.com>
2025-09-28 21:19:19 +0000

    add LoginForm component tests and setup for Vitest
    - Created unit tests for LoginForm component covering submission, validation, and password visibility toggle.
    - Added setup file for testing library to enhance assertions.
    - Updated TypeScript configuration for app and node to support composite projects and testing types.
    - Configured Vite to integrate Vitest for testing with jsdom environment.

--- b2bit-prosel/vite.config.ts
+++ b2bit-prosel/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -6,9 +7,24 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 		},
 	},
+	test: { // O TypeScript vai parar de reclamar desta propriedade
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    css: true,
+  },
 });
+
+// // Vitest configuration
+// export const test = {
+//   globals: true, // Para não precisar importar describe, it, expect, etc. em todo arquivo.
+//   environment: 'jsdom', // Define o ambiente de teste como jsdom.
+//   setupFiles: './src/setupTests.ts', // Arquivo de setup que será executado antes de cada teste.
+//   css: true, // Habilita o processamento de CSS se necessário (útil para CSS Modules, não tanto para Tailwind).
+// };
