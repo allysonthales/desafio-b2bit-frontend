fe5536f38702491e0bd3f85363dc1a4783a414a5
Allyson Thales <121051849+allysonthales@users.noreply.github.com>
2025-09-28 21:40:32 +0000

    Refactor Header and ModeToggle components for improved structure and theme functionality

--- b2bit-prosel/src/components/Header/index.tsx
+++ b2bit-prosel/src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router";
 import { removeToken } from "@/utils/token";
 import { Button } from "../ui/button";
+import { ModeToggle } from "../mode-toggle";
 
 export function Header() {
 	const navigate = useNavigate();
@@ -9,13 +10,14 @@ export function Header() {
 		navigate("/");
 	}
 	return (
-		<header className="flex justify-end  px-4 py-4 bg-white">
+		<header className="flex justify-end items-center gap-2 px-4 py-4 bg-card">
 			<Button
 				className="w-20 h-10 font-semibold sm:w-40 md:w-60 cursor-pointer"
 				onClick={handleClick}
 			>
 				Logout
 			</Button>
+			<ModeToggle />
 		</header>
 	);
 }
--- b2bit-prosel/src/components/ProfileLayout/index.tsx
+++ b2bit-prosel/src/components/ProfileLayout/index.tsx
@@ -4,10 +4,10 @@ import { PageWrapper } from "../PageWrapper";
 
 export function ProfileLayout() {
 	return (
-		<div className="bg-[#F1F5F9] min-h-screen flex flex-col">
+		<div className="bg-background min-h-screen flex flex-col">
 			<Header />
 			<div className="flex flex-1 flex-col justify-center container mx-auto">
-				<PageWrapper className="bg-[#F1F5F9] flex-1 flex items-center justify-center px-4">
+				<PageWrapper className="flex-1 flex items-center justify-center px-4">
 					<Outlet />
 				</PageWrapper>
 			</div>
